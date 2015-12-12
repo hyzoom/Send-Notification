f824ff980351a64d8e3afd4f867c3a940e010052
hyzoom <hazem.abdalazeem@gmail.com>
2015-12-12 21:01:11 +0200

    hazem 20151212

--- build/generated/source/r/debug/com/androidhive/pushnotifications/R.java
+++ build/generated/source/r/debug/com/androidhive/pushnotifications/R.java
@@ -37,6 +37,7 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int activity_register=0x7f030001;
         public static final int send_random_msg=0x7f030002;
+        public static final int splash_screen=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
